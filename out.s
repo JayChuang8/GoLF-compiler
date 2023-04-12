075d288d1d8fe375f9f3c4117639b6ef09df6751
jay.chuang1 <jay.chuang1@csx3.cs.ucalgary.ca>
2023-04-12 15:49:07 -0600

    Add run output

--- out.s
+++ out.s
@@ -45,212 +45,212 @@ S3:
 .text
 G0:
 	.word 0
-G0:
+G1:
 	.word 0
-G0:
+G2:
 	.word 0
-G0:
+G3:
 	.word 0
-G0:
+G4:
 	.word 0
-G0:
+G5:
 	.word 0
-G0:
+G6:
 	.word 0
-G0:
+G7:
 	.word 0
-G0:
+G8:
 	.word 0
-G0:
+G9:
 	.word 0
-G0:
+G10:
 	.word 0
-G0:
+G11:
 	.word 0
-G0:
+G12:
 	.word 0
-G0:
+G13:
 	.word 0
-G0:
+G14:
 	.word 0
-G0:
+G15:
 	.word 0
-G0:
+G16:
 	.word 0
-G0:
+G17:
 	.word 0
-G0:
+G18:
 	.word 0
-G0:
+G19:
 	.word 0
-G0:
+G20:
 	.word 0
-G0:
+G21:
 	.word 0
-G0:
+G22:
 	.word 0
-G0:
+G23:
 	.word 0
-G0:
+G24:
 	.word 0
-G0:
+G25:
 	.word 0
-G0:
+G26:
 	.word 0
-G0:
+G27:
 	.word 0
-G0:
+G28:
 	.word 0
-G0:
+G29:
 	.word 0
-G0:
+G30:
 	.word 0
-G0:
+G31:
 	.word 0
-G0:
+G32:
 	.word 0
-G0:
+G33:
 	.word 0
-G0:
+G34:
 	.word 0
-G0:
+G35:
 	.word 0
-G0:
+G36:
 	.word 0
-G0:
+G37:
 	.word 0
-G0:
+G38:
 	.word 0
-G0:
+G39:
 	.word 0
-G0:
+G40:
 	.word 0
-G0:
+G41:
 	.word 0
-G0:
+G42:
 	.word 0
-G0:
+G43:
 	.word 0
-G0:
+G44:
 	.word 0
-G0:
+G45:
 	.word 0
-G0:
+G46:
 	.word 0
-G0:
+G47:
 	.word 0
-G0:
+G48:
 	.word 0
-G0:
+G49:
 	.word 0
-G0:
+G50:
 	.word 0
-G0:
+G51:
 	.word 0
-G0:
+G52:
 	.word 0
-G0:
+G53:
 	.word 0
-G0:
+G54:
 	.word 0
-G0:
+G55:
 	.word 0
-G0:
+G56:
 	.word 0
-G0:
+G57:
 	.word 0
-G0:
+G58:
 	.word 0
-G0:
+G59:
 	.word 0
-G0:
+G60:
 	.word 0
-G0:
+G61:
 	.word 0
-G0:
+G62:
 	.word 0
-G0:
+G63:
 	.word 0
-G0:
+G64:
 	.word 0
-G0:
+G65:
 	.word 0
-G0:
+G66:
 	.word 0
-G0:
+G67:
 	.word 0
-G0:
+G68:
 	.word 0
-G0:
+G69:
 	.word 0
-G0:
+G70:
 	.word 0
-G0:
+G71:
 	.word 0
-G0:
+G72:
 	.word 0
-G0:
+G73:
 	.word 0
-G0:
+G74:
 	.word 0
-G0:
+G75:
 	.word 0
-G0:
+G76:
 	.word 0
-G0:
+G77:
 	.word 0
-G0:
+G78:
 	.word 0
-G0:
+G79:
 	.word 0
-G0:
+G80:
 	.word 0
-G0:
+G81:
 	.word 0
-G0:
+G82:
 	.word 0
-G0:
+G83:
 	.word 0
-G0:
+G84:
 	.word 0
-G0:
+G85:
 	.word 0
-G0:
+G86:
 	.word 0
-G0:
+G87:
 	.word 0
-G0:
+G88:
 	.word 0
-G0:
+G89:
 	.word 0
-G0:
+G90:
 	.word 0
-G0:
+G91:
 	.word 0
-G0:
+G92:
 	.word 0
-G0:
+G93:
 	.word 0
-G0:
+G94:
 	.word 0
-G0:
+G95:
 	.word 0
-G0:
+G96:
 	.word 0
-G0:
+G97:
 	.word 0
-G0:
+G98:
 	.word 0
-G0:
+G99:
 	.word 0
-G0:
+G100:
 	.word 0
-G0:
+G101:
 	.word 0
 main:
 	jal Lmain
 	j Lhalt
-G0:
+G102:
 	.word 0
 Lmain:
 	subu $sp,$sp,16
@@ -270,6 +270,6 @@ L0for:
 	seq $t2,$t0,$t1
 	beqz $t2,L2else
 	li $t0,0
-	sw $t0,G0
+	sw $t0,G1
 	j L3ifelse
 L2else:
