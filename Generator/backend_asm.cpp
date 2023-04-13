c8e0561e455811787807f57a520a788e0bd67ed2
jay.chuang1 <jay.chuang1@cs.ucalgary.ca>
2023-04-12 22:48:12 -0600

    exit code 1

--- Generator/backend_asm.cpp
+++ Generator/backend_asm.cpp
@@ -177,9 +177,9 @@ void BackendASM::epilogue()
     emit("move $v0, $a1");
     emit("jr $ra");
     emitlabel("divmod_error");
-    emit("li $v0, 4");
+    emit("li $v0, 10");
     emit("la $a0, DivZeroError");
-    emit("li $v0, 1");
+    // emit("li $v0, 1");
     emit("syscall");
 
     emitlabel("exit_with_error");
