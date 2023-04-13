269ca6678467ca98561fde7b8f7f161cf34ef1d1
jay.chuang1 <jay.chuang1@cs.ucalgary.ca>
2023-04-12 22:49:36 -0600

    1

--- Generator/backend_asm.cpp
+++ Generator/backend_asm.cpp
@@ -177,9 +177,8 @@ void BackendASM::epilogue()
     emit("move $v0, $a1");
     emit("jr $ra");
     emitlabel("divmod_error");
-    emit("li $v0, 10");
     emit("la $a0, DivZeroError");
-    // emit("li $v0, 1");
+    emit("li $v0, 10");
     emit("syscall");
 
     emitlabel("exit_with_error");
